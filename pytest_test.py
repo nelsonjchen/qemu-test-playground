62a2c4bb9300ba00b7dfa9a742567ee9fabc00e4
Nelson Chen <nelson.chen@mobivity.com>
2019-01-04 00:33:02 -0800

    It uploads and works much better on Linux

--- pytest_test.py
+++ pytest_test.py
@@ -11,6 +11,7 @@ import time
 
 import pytest
 import winrm
+from smb.SMBConnection import SMBConnection
 
 import qemu
 
@@ -19,21 +20,20 @@ import qemu
 def vm():
     logging.info("Making VM Fixture")
     with qemu.QEMUMachine(
-            r"C:\Program Files\qemu\qemu-system-x86_64.exe",
-            test_dir="test_dir",
-            monitor_address=("127.0.0.1", 8008)
-    ) as vm:
+        binary='qemu-system-x86_64',
+        test_dir='./test_dir',
+) as vm:
         vm.add_args("-m", "2048")
-        vm.add_args("-vnc", ":0")
         vm.add_args("-vga", "std")
         vm.add_args("-snapshot")
         vm.add_args("-device", 'usb-tablet')
         vm.add_args("-netdev", 'user,id=n1,hostfwd=tcp:127.0.0.1:5995-:5985,hostfwd=tcp:127.0.0.1:8445-:445')
-        vm.add_args("-device", 'e1000,netdev=n1,mac=52:54:98:76:54:32')
+        vm.add_args("-device", 'virtio-net-pci,netdev=n1,mac=52:54:98:76:54:32')
         vm.add_args("-machine", 'usb=on')
-        vm.add_args("-accel", 'whpx')
+        vm.add_args("-accel", 'kvm')
+        vm.add_args("-cpu", 'host,hv_relaxed,hv_spinlocks=0x1fff,hv_time,hv_vapic,hv_vendor_id=0xDEADBEEFFF')
         vm.add_args("-vnc", ':1')
-        vm.add_args("--drive", r"file=C:\Users\Nelson\Documents\packer-windows-docker\output-qemu\packer-qemu")
+        vm.add_args("-drive", r"file=/home/nelson/code/packer-windows-docker/output-qemu/packer-qemu,format=raw,if=virtio")
         logging.info("Launching VM")
         vm.launch()
         logging.info("Running Version Info Check")
@@ -52,16 +52,26 @@ def vm():
                 logging.info("dir success")
                 break
             except:
-                logging.exception("dir failed")
+                logging.info("dir failed")
                 time.sleep(10)
                 pass
         # Once command has run, wait 10 seconds.
-        logging.info("running dir again")
-        s.run_cmd('dir')
-        logging.info("ran dir again")
+        logging.debug("sharing desktop")
+        s.run_cmd('net', ['share', r'Desktop=C:\Users\vagrant\Desktop', '/grant:everyone,FULL'])
         logging.info("Yielding VM Fixture")
         yield vm
     logging.info("Tearing down VM Fixture")
 
 def test_install(vm):
+    s = winrm.Session('localhost:5995', ('vagrant', 'vagrant'), read_timeout_sec=20, operation_timeout_sec=10)
+
+    logging.info("uploading installer.msi")
+    c = SMBConnection('vagrant', 'vagrant', 'PYTHON', 'vagrant-10', is_direct_tcp=True)
+    c.connect('127.0.0.1', port=8445)
+    with open('installer/installer.msi', 'rb') as f:
+        c.storeFile('Desktop', 'installer.msi', f)
+    c.close()
+    logging.info("closed smb")
+    logging.info("Sleeping 600 seconds")
+    time.sleep(60*10)
     assert True
--- simple_test.py
+++ simple_test.py
@@ -1,9 +1,6 @@
 import qemu
 
-with qemu.QEMUMachine(
-        r"C:\Program Files\qemu\qemu-system-x86_64.exe",
-        test_dir="test_dir",
-        monitor_address=("127.0.0.1", 8008)
+with qemu.QEMUMachine(binary='qemu-system-x86_64'
 ) as vm:
     vm.launch()
     res = vm.command('human-monitor-command',
